b90bb2e4712dec71dd17b96afce3178f8e8745fc
Polyakov Ilya <wwwtrayfrost@gmail.com>
2022-02-06 07:24:14 +0300

    Removed comment

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,3 @@ export * from './constants'
 export * from './requests'
 
 export { Bitfield } from './utils/classes/Bitfield.class'
-
-
-// getStatusSmart('rujka.ru:29070').then((result) => {
-//   console.log(result);
-// }).catch((err) => {
-//   console.error(err);
-// });
