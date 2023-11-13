082c3aaf7c3bfbd6ea0ac6cdd9da26d754acfc05
Ilia Poliakov <ilia.poliakov@macys.com>
2023-11-13 21:22:21 +0400

    Adding jest for testing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/src'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
